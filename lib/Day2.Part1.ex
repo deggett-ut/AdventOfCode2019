1635e580f4e3664d8389f20f2c8a6ae41f87ea7d
Darren Eggett <darren_eggett@bose.com>
2019-12-04 00:41:39 -0700

    Completed -- refadtor processing function into two parts in preparation for part 2

--- lib/Day2.Part1.ex
+++ lib/Day2.Part1.ex
@@ -1,4 +1,4 @@
-defmodule Day2.Part1 do
+defmodule Day2 do
   @doc """
   Process a list of integers as
   Intcode input.  Details on Intcode
@@ -9,22 +9,22 @@ defmodule Day2.Part1 do
 
   #Examples
 
-  iex> Day2.Part1.process_intcode([1, 0, 0, 3, 99])
+  iex> Day2.process_intcode([1, 0, 0, 3, 99])
   [1, 0, 0, 2, 99]
 
-  iex> Day2.Part1.process_intcode([2, 0, 0, 3, 99])
+  iex> Day2.process_intcode([2, 0, 0, 3, 99])
   [2, 0, 0, 4, 99]
 
-  iex> Day2.Part1.process_intcode([1, 0, 1, 2, 99])
+  iex> Day2.process_intcode([1, 0, 1, 2, 99])
   [1, 0, 1, 2, 99]
 
-  iex> Day2.Part1.process_intcode([2, 2, 1, 0, 99])
+  iex> Day2.process_intcode([2, 2, 1, 0, 99])
   [2, 2, 1, 0, 99]
 
-  iex> Day2.Part1.process_intcode([1,9,10,3,2,3,11,0,99,30,40,50])
+  iex> Day2.process_intcode([1,9,10,3,2,3,11,0,99,30,40,50])
   [3500,9,10,70,2,3,11,0,99,30,40,50]
 
-  iex> Day2.Part1.process_intcode([1,1,1,4,99,5,6,0,99])
+  iex> Day2.process_intcode([1,1,1,4,99,5,6,0,99])
   [30,1,1,4,2,5,6,0,99]
 
   """
@@ -52,19 +52,23 @@ defmodule Day2.Part1 do
   defp do_action(2, v1, v2), do: v1 * v2
 
   def process_file(file) do
+    get_intcode_list_from_file(file)
+    |> process_list(12, 2)
+  end
+
+  def get_intcode_list_from_file(file) do
     file
     |> File.read!()
     |> String.trim()
     |> String.split(~r/,/, trim: true)
     |> Enum.map(&String.to_integer/1)
-    |> process_list(12, 2)
   end
 
   def process_list(input, noun, verb) do
     input
     |> List.replace_at(1, noun)
     |> List.replace_at(2, verb)
-    |> Day2.Part1.process_intcode()
+    |> Day2.process_intcode()
     |> List.first()
   end
 end
