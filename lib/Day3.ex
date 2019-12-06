34e7d13bbd9de1d7ddc171935d5dd66857461d3f
Darren Eggett <deggett@deggett.com>
2019-12-05 20:51:19 -0700

    Both manhattan and step-count seem to be working

--- lib/Day3.ex
+++ lib/Day3.ex
@@ -1,15 +1,20 @@
 defmodule Day3 do
   alias Day3.WireCreator
-  alias Day3.Grid.Part1, as: Grid
+  alias Day3.Grid.Part2, as: Grid
 
   @input_file Path.join("test", "Day3-input.txt")
 
-  def process_input_file(file_name \\ @input_file) do
+  def process_input_file(file_name \\ @input_file, algorithm \\ :steps) do
     file_name
     |> break_file_into_lines()
     |> WireCreator.turn_text_lines_into_wires()
     |> Grid.place_wires_on_grid()
-    |> Grid.find_closest_intersection_point()
+    |> (fn x ->
+          case algorithm do
+            :manhattan -> Grid.find_closest_intersection_point(x)
+            :steps -> Grid.find_intersection_point_by_steps_taken(x)
+          end
+        end).()
   end
 
   def break_file_into_lines(file_name) do
