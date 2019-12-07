4480d469dcb266428e380666093b98b6e25eeb0c
Darren Eggett <deggett@deggett.com>
2019-12-06 19:04:31 -0700

    Day5 -- copied Day2 and changed from working with Enum.chunk to using the chunk_index as the index into the list being processed.

---
+++ lib/Day5.ex
@@ -0,0 +1,81 @@
+defmodule Day5 do
+  @doc """
+  Process a list of integers as
+  Intcode input.  Details on Intcode
+  processing are found on the Advent
+  of Code website here:
+
+  https://adventofcode.com/2019/day/2
+
+  #Examples
+
+  iex> Day5.process_intcode([1, 0, 0, 3, 99])
+  [1, 0, 0, 2, 99]
+
+  iex> Day5.process_intcode([2, 0, 0, 3, 99])
+  [2, 0, 0, 4, 99]
+
+  iex> Day5.process_intcode([1, 0, 1, 2, 99])
+  [1, 0, 1, 2, 99]
+
+  iex> Day5.process_intcode([2, 2, 1, 0, 99])
+  [2, 2, 1, 0, 99]
+
+  iex> Day5.process_intcode([1,9,10,3,2,3,11,0,99,30,40,50])
+  [3500,9,10,70,2,3,11,0,99,30,40,50]
+
+  iex> Day5.process_intcode([1,1,1,4,99,5,6,0,99])
+  [30,1,1,4,2,5,6,0,99]
+
+  """
+  def process_intcode(input, chunk_index \\ 0) do
+    get_next_chunk(input, chunk_index)
+    |> execute_chunk(input, chunk_index)
+  end
+
+  defp get_next_chunk(input, chunk_index) do
+    {_, chunk} = Enum.split(input, chunk_index)
+
+    chunk
+  end
+
+  defp execute_chunk([99 | _], input, _) do
+    # the base case -- becomes the return value for process_intcode()
+    input
+  end
+
+  defp execute_chunk([op, noun, verb, result_index | _], input, chunk_index)
+       when op == 1 or op == 2 do
+    value1 = Enum.at(input, noun)
+    value2 = Enum.at(input, verb)
+    result = do_action(op, value1, value2)
+
+    input
+    |> List.replace_at(result_index, result)
+    # loop back to entry point with changed list
+    |> process_intcode(chunk_index + 4)
+  end
+
+  defp do_action(1, v1, v2), do: v1 + v2
+  defp do_action(2, v1, v2), do: v1 * v2
+
+  def process_file(file) do
+    get_intcode_list_from_file(file)
+    |> process_intcode_list(12, 2)
+  end
+
+  def get_intcode_list_from_file(file) do
+    file
+    |> File.read!()
+    |> String.split(~r/,/, trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def process_intcode_list(input, noun, verb) do
+    input
+    |> List.replace_at(1, noun)
+    |> List.replace_at(2, verb)
+    |> process_intcode()
+    |> List.first()
+  end
+end
